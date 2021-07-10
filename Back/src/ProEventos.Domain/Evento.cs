8f57e160c96b1774cb3bef5a61b8bf3a39f87f02
RohgerPereira <rogerpereira1@live.com>
2021-07-10 17:49:38 -0300

    Seção 6 .NET Core - Organizando Camadas

---
+++ Back/src/ProEventos.Domain/Evento.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using ProEventos.Domain;
+
+namespace ProEventos.Domain
+
+{
+    public class Evento
+    {
+        public int  Id {get; set; }
+        public string Local {get; set; }
+        public  DateTime? DataEvento {get; set; }
+
+        public string Tema {get; set; }
+
+        public int QtdPessoas {get; set; }
+
+        public string ImagemUrl {get; set; }
+
+        public String Telefone {get; set; }
+
+        public String Email { get; set; }    
+
+        public  IEnumerable<Lote> Lotes { get; set; }
+
+        public  IEnumerable<RedeSocial> RedesSociais { get; set; }
+
+        public IEnumerable<PalestranteEvento> PalestrantesEventos { get; set; }
+
+    }
+}
